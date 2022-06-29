1c10f6a7bde79351de2c58940e0236a292cc7758
hzz780 <hzz780@qq.com>
2022-06-29 22:45:43 +0800

    fix: 🐛 remove duplicate length

--- src/schema.js
+++ src/schema.js
@@ -36,7 +36,7 @@ const typeDefs = gql`
     "The track's illustration to display in track card or track page detail"
     thumbnail: String
     "The track's approximate length to complete, in minutes"
-    length: Int
+    length: Int @deprecated(reason: "Use durationInSeconds")
     "The number of modules this track contains"
     modulesCount: Int
     "The track's complete description, can be in markdown format"
@@ -46,7 +46,6 @@ const typeDefs = gql`
     "The track's complete array of Modules"
     modules: [Module!]!
     "The track's approximate length to complete, in seconds"
-    length: Int @deprecated(reason: "Use durationInSeconds")
     durationInSeconds: Int
     
   }
@@ -66,13 +65,12 @@ const typeDefs = gql`
     "The module's title"
     title: String!
     "The module's length in minutes"
-    length: Int
+    length: Int @deprecated(reason: "Use durationInSeconds")
     "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
     content: String
     "The module's video url, for video-based modules"
     videoUrl: String
     "The module's length in seconds"
-    length: Int @deprecated(reason: "Use durationInSeconds")
     durationInSeconds: Int
   }
 `;
